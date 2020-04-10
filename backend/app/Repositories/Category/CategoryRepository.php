7a28a76fd53b882fe33231537dcc6f1bee93ebe3
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-04-10 16:46:22 -0300

    Creado nuevo repositorio de categorias y su correspondiente modelo.

---
+++ backend/app/Repositories/Category/CategoryRepository.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Repositories\Catergory;
+use App\Models\Category;
+
+class CategoryRepository {
+    public static function findById($id){
+        return Category::find($id);
+    }
+    
+    public static function findAll(){
+        return Category::all();
+    }
+
+    public static function delete($id){
+        Category::detroy($id);
+    }
+
+    public static function update($id,$params){
+        $category = self::findById($id);
+        $formatArrayData=json_decode($params,true); 
+        //Function update need an array
+        $category->update($formatArrayData);
+
+    }
+
+    public static function insert($params){
+        Category::create($params);
+    }
+
+}
