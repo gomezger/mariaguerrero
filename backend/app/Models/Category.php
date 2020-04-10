19f14cb8dec83119903259a26de85479b8eaeb2d
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-04-10 19:16:17 -0300

    Modelos cargados.

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table='categories';
+
+    public function products(){
+        return $this->HasMany('App\Models\Product','product_id','id');
+    }
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+    public function category(){
+        return $this->HasOne('App\Models\Category','id','category_id');
+    }
 }
