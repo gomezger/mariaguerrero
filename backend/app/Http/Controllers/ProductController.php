bef8eb78a0f9322012a06f1cb63cfe8d1ae58e85
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-04-13 17:00:59 -0300

    Controlador de Productos con los metodos privados listos.

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -31,4 +31,49 @@ class ProductController extends Controller
         return Response::success('Producto subido','producto',$product);
         
     }
+
+    public function getAll(){
+        $products=ProductRepo::findAll();
+        return Response::success('Productos listos','productos',$products);
+    }
+
+    public function getById($id){
+        $product = ProductRepo::findById($id);
+        return Response::success('Producto subido','producto',$product);
+    }
+
+    public function update($id,Request $request){
+        //get data
+        $requestParams = json_decode($request->input('json'),true);
+
+        //Validate data
+        FileValidator::validate($request->file('file'),'mimes:jpeg,gif,png|required');
+        FormProductValidator::validate($requestParams);
+
+        //upload image
+        $requestParams['images']  = array();
+        array_push($requestParams['images'],FileUploader::upload($request->file('file'),'public'));
+        $requestParams['images'] = json_encode($requestParams['images']);
+
+        ProductRepo::update($id,$requestParams);
+    }
+
+    public function delete($id){
+        ProductRepo::delete($id);
+    }    
+
+    private function getData(Request $request){
+        $requestParams = json_decode($request->input('json'),true);
+    }
+
+    private function validateData(Request $request, $requestParams){
+        FileValidator::validate($request->file('file'),'mimes:jpeg,gif,png|required');
+        FormProductValidator::validate($requestParams);
+    }
+
+    private function uploadImage($requestParams){
+        $requestParams['images']  = array();
+        array_push($requestParams['images'],FileUploader::upload($request->file('file'),'public'));
+        $requestParams['images'] = json_encode($requestParams['images']);
+    }
 }
