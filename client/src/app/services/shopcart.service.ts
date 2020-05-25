c727c7ef3a758a7da2f10e7d68939ac69c6581d1
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-05-24 23:09:31 -0300

    Vista de presupuestos maquetada tomando datos del local storage

--- client/src/app/services/shopcart.service.ts
+++ client/src/app/services/shopcart.service.ts
@@ -8,20 +8,36 @@ export class ShopcartService {
 
   constructor() { }
 
-  saveProduct(productToSave :Product){
-    if (localStorage.getItem('presupuesto')){
-      let  presupuesto= []
-      presupuesto  = JSON.parse(localStorage.getItem('presupuesto'));
+  saveProduct(productToSave :Product,cant){
+    if (localStorage.getItem('presupuesto')&& localStorage.getItem('cantidades')){
+      let presupuesto= [];
+      let cantidades = [];
+      presupuesto = JSON.parse(localStorage.getItem('presupuesto'));
+      cantidades = JSON.parse(localStorage.getItem('cantidades'));
       presupuesto.push(productToSave);
+      cantidades.push(cant);
+      localStorage.setItem('presupuesto',JSON.stringify(presupuesto));
+      localStorage.setItem('cantidades',JSON.stringify(cantidades));
     }else{
       let save = [productToSave];
+      let cantidad= [cant];
       localStorage.setItem('presupuesto',JSON.stringify(save));
+      localStorage.setItem('cantidades',JSON.stringify(cantidad));
     }
   }
 
   getProducts(){
     if (localStorage.getItem('presupuesto')){
-      return JSON.parse(localStorage.getItem('presupuesto'))
+      return JSON.parse(localStorage.getItem('presupuesto'));
     }
   }
+
+  getCantidades(){
+    if (localStorage.getItem('cantidades')){
+      return JSON.parse(localStorage.getItem('cantidades'));
+    }
+  }
+
+
+
 }
