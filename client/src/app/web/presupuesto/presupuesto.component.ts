a8928071d99cc8ec03cb37e9b667b3f96e99a3c1
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2020-05-25 20:27:24 -0300

    Presupuesto actualizado.

--- client/src/app/web/presupuesto/presupuesto.component.ts
+++ client/src/app/web/presupuesto/presupuesto.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ShopcartService} from '../../services/shopcart.service'
-
+import {ShopcartService} from '../../services/shopcart.service';
+import {MailSenderService} from 'src/app/services/mail-sender.service';
+import {Contacto}from '../../models/contacto';
 import {Product} from '../../models/product';
-import {Category}from '../../models/category';
 import { Title } from '@angular/platform-browser';
+import {Category}from 'src/app/models/category';
 
 @Component({
   selector: 'app-presupuesto',
@@ -14,23 +15,26 @@ export class PresupuestoComponent implements OnInit {
 
   public presupuesto:Product [];
   public cantidades:[];
-
+  public myContacto:Contacto;
   constructor(
     private _shopcart:ShopcartService,
-    private _title: Title
-  ) { }
+    private _title: Title,
+    private _mailSender:MailSenderService
+  ) { 
+    this.myContacto=new Contacto('','','','');
+  }
 
   ngOnInit(): void {
-    // let p1:Product;
-    // let p2:Product;
-    // let p3:Product;
-    // p1 = new Product(1,'Producto Prueba 1',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
-    // p2 = new Product(1,'Producto Prueba 2',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
-    // p3 = new Product(1,'Producto Prueba 3',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
+    let p1:Product;
+    let p2:Product;
+    let p3:Product;
+    p1 = new Product(1,'Producto Prueba 1',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
+    p2 = new Product(1,'Producto Prueba 2',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
+    p3 = new Product(1,'Producto Prueba 3',1,new Category(1,'test',null,null),null,'sadasjdgasjdgadads',null,null);
 
-    // this._shopcart.saveProduct(p1,2);
-    // this._shopcart.saveProduct(p2,1);
-    // this._shopcart.saveProduct(p3,5);
+    this._shopcart.saveProduct(p1,2);
+    this._shopcart.saveProduct(p2,1);
+    this._shopcart.saveProduct(p3,5);
 
     this.presupuesto= this._shopcart.getProducts();
     this.cantidades =this._shopcart.getCantidades();
@@ -47,7 +51,10 @@ export class PresupuestoComponent implements OnInit {
     this.presupuesto.splice(i,1);
     this.cantidades.splice(i,1);
     this._shopcart.eliminar(i);
+  }
 
+  enviarPresupuestoMail(){
+    this._mailSender.sendMailPresupuesto(this.myContacto.nombre,this.myContacto.from,this.myContacto.phone,this.presupuesto).subscribe();
   }
 
 }
